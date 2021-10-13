93deec67f9b4c88613f7656361bf6c0d596aa8ee
joery12 <90188965+joery12@users.noreply.github.com>
2021-10-13 19:23:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const age = 33;
+if (age >= 18) {
+    console.log("Je kunt naar binnen gaan.");
+} else {
+    console.log("Sorry maar je bent nog niet oud genoeg.");
+}
+
+const isFemale = true;
+if (isFemale) {
+    console.log("Je hebt geluk het is ladiesnight.");
+} else {
+    console.log("Sorry maar het is vanavond ladiesnight.");
+}
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("Je bent BOB, je mag naar huis rijden.");
+} else {
+    console.log("Sorry je hebt teveel gedronken om nog te mogen rijden");
+}
